cf06609cfe731ab0b5d10b9cfb5251eaa3c9a43f
minju0809 <loverminju88@naver.com>
2022-09-07 23:23:11 +0900

    4주차 ~2/4

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -21,7 +21,6 @@ const badgeEl = document.querySelector('header .badges');
 
 // _.throttle(함수, 시간) 일정 시간에 한 번씩만 작동하도록 제한을 검
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.scrollY);
   if (window.scrollY > 500) {
     // 배지 숨기기
     // gsap.to(요소, 지속시간, 옵션);
@@ -87,4 +86,31 @@ promotionToggleBtn.addEventListener('click', function () {
     // 보임 처리!
     promotionEl.classList.remove('hide');
   }
-})
+});
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject (selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+      y: size,
+      repeat: -1, // 무한 반복
+      yoyo: true, // 재생 후 뒤로 재생
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    }
+  )
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
